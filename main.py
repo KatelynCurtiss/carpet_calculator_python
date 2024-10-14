ff3c640a5e5944b338273279471f020b5ff28674
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-10-14 10:55:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,22 @@
 length = int(input("Enter the length of the room: " ))
 width = int(input("Enyter the width of the room in feet: "))
 price = float(input("Enter the price per square yard of carpet"))
-salestax_rate = 0.06
+SALES_TAX_RATE = 0.06
 
 square_yards = (length * width) / 9
-print(f"You need" + str(square_yards) + " square yards of carpet.")
+# print(f"You need" + str(square_yards) + " square yards of carpet.")
+print(f"You need {square_yards:.2f} square yards of carpet.")
 
 subtotal = square_yards * price
-print(f"Your subtotal is" + str(subtotal)+ ".")
+#print(f"Your subtotal is" + str(subtotal)+ ".")
+print(f"Your subtotal is ${subtotal:.2f}.")
 
-sales_tax = salestax_rate
+# sales_tax = salestax_rate
+sales_tax = SALES_TAX_RATE * subtotal
 
 grandtotal = subtotal + sales_tax
-print(f"The sales tax is" + str(sales_tax) + ".")
-print(f"Your grand total is" + str(grandtotal) + ".")
+# print(f"The sales tax is" + str(sales_tax) + ".")
+# print(f"Your grand total is" + str(grandtotal) + ".")
+
+print(f"The sales tax is ${sales_tax:.2f}.")
+print(f"Your grand total is ${grandtotal:.2f}.")
